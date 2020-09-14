d60df8646b2421753f0a8f051fac4232cddf12d5
huzhengchao <huzhengchao01@saicmotor.com>
2020-09-14 18:17:34 +0800

    增加mybatis测试用例，分析源码需要

---
+++ src/test/java/com/jiagouedu/UserMapper.java
@@ -0,0 +1,42 @@
+/**
+ *    Copyright 2009-2015 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.jiagouedu;/*
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　永无BUG 　┣┓
+ * 　　　　┃　　如来保佑　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┗┻┛　┗┻┛
+ * 图灵学院-悟空老师
+ * www.jiagouedu.com
+ * 悟空老师QQ：245553999
+ */
+
+import org.apache.ibatis.annotations.Select;
+
+public interface UserMapper {
+/*   @Results({
+     @Result(property = "wukong",column = "desc")
+   })*/
+  @Select("select id,username,age,phone,`desc` from user where id=#{id}")
+  public User selectUser(Integer id);
+
+}
